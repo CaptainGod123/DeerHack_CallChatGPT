c7e69bb196d9e341a1eaeab8b6a7ef564dd417ad
Kaitian Zheng <kaitian.zheng@mail.utoronto.ca>
2023-04-29 04:23:03 -0400

    Update chatgpt.py

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -2,18 +2,30 @@ import openai
 
 openai.api_key = ""
 
-# messages = [{"role": "system", "content": "You are a helpful assistant."}]
+MODEL = "gpt-3.5-turbo"
+requests = [{"role": "system", "content": "You are a helpful assistant who provides answers within 50 words"}]
 
-chat = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ]
-)
+def get_response(request):
+    global MODEL
+    global requests
+    requests.append({"role": "user", "content": request})
+    chat = openai.ChatCompletion.create(
+        model = MODEL,
+        messages = requests
+    )
+    reply = chat.choices[0].message.content
+    return reply
 
-reply = chat.choices[0].message.content
+# chat = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "Who won the world series in 2020?"},
+#         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+#         {"role": "user", "content": "Where was it played?"}
+#     ]
+# )
 
-print(reply)
+# reply = chat.choices[0].message.content
+
+# print(reply)
