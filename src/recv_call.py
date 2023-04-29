c52c3bf5bea2ce1c1924159782a68afcaa26bc93
Kaitian Zheng <kaitian.zheng@mail.utoronto.ca>
2023-04-29 04:22:20 -0400

    Update recv_call.py

--- src/recv_call.py
+++ src/recv_call.py
@@ -1,26 +1,46 @@
 from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse, Gather, Say
+from chatgpt import get_response
 
 app = Flask(__name__)
 
+def welcome():
+    response = VoiceResponse()
+
+INITIAL_CONVERSATION = True
 
 @app.route("/answer", methods=['GET', 'POST'])
 def answer_call():
-    # """Respond to incoming phone calls with a brief message."""
-    # # Start our TwiML response
-    # resp = VoiceResponse()
-
-    # # Read a message aloud to the caller
-    # resp.say("Thank you for calling! Have a great day.", voice='alice')
-
-    # return str(resp)
-
+    """Respond to incoming phone calls with a brief message."""
+    global INITIAL_CONVERSATION
     response = VoiceResponse()
-    gather = Gather(input='speech dtmf', action='/complete', speechModel="phone_call")
-    gather.say('Welcome to Twilio')
+    if not INITIAL_CONVERSATION:
+        user_request = parse_request()
+        print(f"user: {user_request}")
+        if user_request is None:
+            response.say("failed")
+            return str(response)
+        else:
+            gpt_response = get_response(user_request)
+            print(f"gpt: {gpt_response}")
+            response.say(gpt_response)
+            response.pause(length=1)
+
+    gather = Gather(input='speech dtmf', action='/answer', speechModel="phone_call")
+    gather.say('How can I help you?' if INITIAL_CONVERSATION else 'Do you have any furthur questions?')
     response.append(gather)
 
+    # response.say("Received")
+
+    INITIAL_CONVERSATION = False
+
     return str(response)
+
+def parse_request():
+    speech_result = request.form["SpeechResult"]
+    confidence = request.form["Confidence"]
+    print(confidence)
+    return speech_result
 
 
 @app.route("/complete", methods=['POST'])
